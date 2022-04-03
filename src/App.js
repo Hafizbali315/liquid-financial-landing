9468899911330338bc52755c2f264796e5a28524
hafizbali315 <hafizbali315@gmail.com>
2022-04-04 02:12:11 +0500

    Some Fixes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-      <Router>
-
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+				</Routes>
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
